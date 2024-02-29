3e6f1a307d105e6cc78916215641e378692d44c3
Tim <timblazic.info@gmail.com>
2024-02-29 21:22:54 +0100

    /

--- app/page.tsx
+++ app/page.tsx
@@ -34,8 +34,8 @@ export default async function Dashboard() {
   const fullName = userDetails?.full_name && userDetails.full_name;
 
   return (
-    <div className="flex h-screen">
-      <div className="bg-white shadow-xl w-1/5 m-5 h-5/6 py-8 flex flex-col items-center justify-center border border-white rounded-xl">
+    <div className="flex flex-col lg:flex-row h-screen">
+      <div className="bg-white shadow-lg lg:w-1/3 lg:m-auto m-5 p-8 rounded-xl flex flex-col items-center justify-center border border-gray-200">
         <div className="mb-6">
           <img
             className="w-32 h-32 object-cover rounded-full"
@@ -43,21 +43,19 @@ export default async function Dashboard() {
             alt="Profile"
           />
         </div>
-
         <h2 className="text-lg text-gray-800 font-bold">{fullName}</h2>
-
         <Link
           href="/account"
-          className="text-gray-500 mt-2 hover:text-gray-700 border shadow-lg rounded-xl py-2 px-10 focus:outline-none focus:text-gray-700"
+          className="text-gray-500 mt-2 hover:text-gray-700 border border-gray-200 shadow-md rounded-lg py-2 px-6 focus:outline-none focus:text-gray-700"
         >
           Profil
         </Link>
       </div>
-      <div className="bg-white shadow-xl w-1/4 m-5 h-1/4 py-8 flex flex-col items-center justify-center border border-white rounded-xl">
+      <div className="bg-white shadow-lg lg:w-1/3 lg:m-auto m-5 p-8 rounded-xl flex flex-col items-center justify-center border border-gray-200">
         <Link
           href="/selection"
           passHref
-          className="bg-white text-black h-1 w-1/5 py-24 text-center text-2xl m-5 font-bold uppercase items-center justify-center"
+          className="bg-white text-black h-20 w-20 py-5 text-center text-2xl m-5 font-bold uppercase rounded-lg focus:outline-none focus:text-gray-700"
         >
           Kviz
         </Link>
--- app/quiz/results/[quizId]/page.tsx
+++ app/quiz/results/[quizId]/page.tsx
@@ -1,29 +1,71 @@
 'use client';
 
+import Button from '@/components/ui/Button';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import supabase from 'utils/supabaseClient';
 
 const ResultsPage: React.FC<{ params: { quizId: string } }> = ({ params }) => {
   const [questionsData, setQuestionsData] = useState<any[]>([]);
+  const [score, setScore] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
-        // Fetch questions from the 'quiz_questions' table based on the quizId
-        const { data: fetchedQuestions, error } = await supabase
+        const { data: fetchedQuestions, error: questionsError } = await supabase
           .from('quiz_questions')
           .select('question_id, order_in_quiz')
           .eq('quiz_id', params.quizId);
 
-        if (error) {
+        if (questionsError) {
           setError('Error fetching questions');
           setLoading(false);
           return;
         }
 
-        setQuestionsData(fetchedQuestions || []);
+        const questionsDetails = await Promise.all(
+          fetchedQuestions.map(async (question: any) => {
+            const { data: questionData, error: questionError } = await supabase
+              .from('questions')
+              .select(
+                'question_text, subject, points, options, correct_answer, picture'
+              )
+              .eq('question_id', question.question_id)
+              .single();
+
+            if (questionError) {
+              throw new Error('Error fetching question details');
+            }
+
+            // Fetch the answer for the current question from the 'quizzes' table
+            const { data: quizData, error: quizError } = await supabase
+              .from('quizzes')
+              .select('answer, score') // Also fetch the score
+              .eq('quiz_id', params.quizId)
+              .single();
+
+            if (quizError) {
+              throw new Error('Error fetching quiz data');
+            }
+
+            // Update the score
+            setScore(quizData.score);
+
+            const userAnswerIndex = question.order_in_quiz - 1; // Adjust for zero-based indexing
+            const userAnswer = quizData.answer[userAnswerIndex];
+
+            return {
+              ...questionData,
+              order_in_quiz: question.order_in_quiz,
+              // Store the answer from the quiz in each question
+              answer: userAnswer
+            };
+          })
+        );
+
+        setQuestionsData(questionsDetails);
         setLoading(false);
       } catch (error) {
         console.error('Error fetching questions:', error.message);
@@ -35,27 +77,77 @@ const ResultsPage: React.FC<{ params: { quizId: string } }> = ({ params }) => {
     fetchQuestions();
   }, [params.quizId]);
 
+  useEffect(() => {
+    console.log('Questions Data:', questionsData);
+  }, [questionsData]);
+
   if (loading) {
-    return <div>Loading...</div>;
+    return <div className="container mx-auto">Loading...</div>;
   }
 
   if (error) {
-    return <div>Error: {error}</div>;
+    return <div className="container mx-auto">Error: {error}</div>;
   }
 
   return (
-    <div>
-      <h1>Rezultati</h1>
-      <p>Quiz ID: {params.quizId}</p>
-      <h2>Questions:</h2>
-      <ul>
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold mb-4">Rezultati</h1>
+      <p className="text-lg font-medium">Točke: {score}</p>{' '}
+      <ul className="space-y-8">
         {questionsData.map((question, index) => (
-          <li key={index}>
-            Question ID: {question.question_id}, Order in Quiz:{' '}
-            {question.order_in_quiz}
+          <li key={index} className="bg-white rounded-lg shadow-md p-8">
+            <p className="text-xl font-semibold">
+              {index + 1}. {question.question_text}
+            </p>
+            <p className="text-lg font-medium">Točke: {question.points}</p>
+            {question.picture && (
+              <div className="mt-4">
+                <img
+                  className="mt-2"
+                  src={question.picture}
+                  alt={`Question ${index + 1}`}
+                />
+              </div>
+            )}
+            <div className="mt-2">
+              <ul className="ml-6 list-disc">
+                {question.options.map((option: string, optionIndex: number) => {
+                  const isCorrectOption = option === question.correct_answer;
+                  const isSelectedOptionCorrect = option === question.answer;
+
+                  return (
+                    <li
+                      key={optionIndex}
+                      className={`font-bold text-base ${
+                        isSelectedOptionCorrect
+                          ? isCorrectOption
+                            ? 'text-green-500' // Selected and correct
+                            : 'text-red-500' // Selected but incorrect
+                          : '' // Not selected
+                      }`}
+                    >
+                      {option}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <p className="text-lg font-medium mt-2">
+              Pravilni odgovor: {question.correct_answer}
+            </p>
           </li>
         ))}
       </ul>
+      <div className="flex justify-center mt-8">
+        <Button>
+          <Link
+            href={`/`}
+            className="bg-primary font-semibold py-3 px-6 rounded-lg inline-block transition duration-300 hover:bg-primary-dark"
+          >
+            Domov
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 };
